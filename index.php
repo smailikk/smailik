a41d858069842bd0d674904a19852ab247554923
smailikk <32063140+smailikk@users.noreply.github.com>
2017-11-26 09:10:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,248 @@
-<!DOCTYPE html>
-<html>
+    <!doctype html>
+<html class="no-js" lang="en">
   <head>
-    <meta charset="utf-8">
-    <title>smailik</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Покупка и продажа автомобилей</title>
+    <link rel="stylesheet" href="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
   </head>
   <body>
+
+    <!-- Start Top Bar -->
+    <div class="top-bar">
+      <div class="top-bar-left">
+        <ul class="menu">
+          <li class="menu-text">Автомобили</li>
+        </ul>
+      </div>
+      <div class="top-bar-right">
+        <ul class="menu">
+          <li><a href="#">Войти</a></li>
+          <li><a href="file:///C:/Users/Acer/Downloads/all-f6-templates/all-f6-templates/регистрация">Регистрация</a></li>
+        </ul>
+      </div>
+    </div>
+    <!-- End Top Bar -->
+
+
+    <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+      <ul class="orbit-container">
+        <li class="orbit-slide is-active">
+          <img src="https://www.bmw.ru/content/dam/bmw/marketRU/bmw_ru/images/xDrive/BMW_Line_volcano_11_flat_wip2_clean-1680.jpg/_jcr_content/renditions/cq5dam.resized.img.1680.large.time1510244728542.jpg">
+        </li>
+      </ul>
+    </div>
+
+    <div class="row column text-center">
+      <h2>Купи новый автомобиль</h2>
+      <hr>
+    </div>
+
+    <div class="row small-up-2 large-up-4">
+      <div class="column">
+        <img class="thumbnail" src="https://www.audi.ru/content/dam/nemo/ru/models/a7/a7-sportback/my-2017/1920x1080-gallery/1920x1080_A7_1410102.jpg">
+        <h5>Audi A7 Sportback</h5>
+        <p>$2 000 000</p>
+        <a href="#" class="button expanded">Buy</a>
+      </div>
+      <div class="column">
+        <img class="thumbnail" src="http://narule.ru/files/new_cars/576/1204/w430_img_model.jpg">
+        <h5>Mercedes-Benz C-класс Купе</h5>
+        <p>$2 850 000</p>
+        <a href="#" class="button expanded">Buy</a>
+      </div>
+      <div class="column">
+        <img class="thumbnail" src="http://wp1.carwallpapers.ru/bmw/1/2017-m140i-xdrive-edition-shadow-5d/BMW-M140i-xDrive-Edition-Shadow-5door-2017-1920x1080-004.jpg">
+        <h5>BMW 1 СЕРИИ</h5>
+        <p>$1 400 000</p>
+        <a href="#" class="button expanded">Buy</a>
+      </div>
+      <div class="column">
+        <img class="thumbnail" src="https://media.ed.edmunds-media.com/lexus/rx-350/2016/oem/2016_lexus_rx-350_4dr-suv_base_fq_oem_10_1280.jpg">
+        <h5>Лексус рх 350</h5>
+        <p>$2 000 400</p>
+        <a href="#" class="button expanded">Buy</a>
+      </div>
+    </div>
+
+    <hr>
+
+    <div class="row column text-center">
+      <div class="callout primary">
+        <h3>Действительно большие скидки</h3>
+      </div>
+    </div>
+
+    <hr>
+
+    <div class="row column text-center">
+      <h2>Some Other Neat Products</h2>
+      <hr>
+    </div>
+
+    <div class="row small-up-2 medium-up-3 large-up-6">
+      <div class="column">
+        <img class="thumbnail" src="http://placehold.it/300x400">
+        <h5>Nulla At Nulla Justo, Eget</h5>
+        <p>$400</p>
+        <a href="#" class="button small expanded hollow">Buy</a>
+      </div>
+      <div class="column">
+        <img class="thumbnail" src="http://placehold.it/300x400">
+        <h5>Nulla At Nulla Justo, Eget</h5>
+        <p>$400</p>
+        <a href="#" class="button small expanded hollow">Buy</a>
+      </div>
+      <div class="column">
+        <img class="thumbnail" src="http://placehold.it/300x400">
+        <h5>Nulla At Nulla Justo, Eget</h5>
+        <p>$400</p>
+        <a href="#" class="button small expanded hollow">Buy</a>
+      </div>
+      <div class="column">
+        <img class="thumbnail" src="http://placehold.it/300x400">
+        <h5>Nulla At Nulla Justo, Eget</h5>
+        <p>$400</p>
+        <a href="#" class="button small expanded hollow">Buy</a>
+      </div>
+      <div class="column">
+        <img class="thumbnail" src="http://placehold.it/300x400">
+        <h5>Nulla At Nulla Justo, Eget</h5>
+        <p>$400</p>
+        <a href="#" class="button small expanded hollow">Buy</a>
+      </div>
+      <div class="column">
+        <img class="thumbnail" src="http://placehold.it/300x400">
+        <h5>Nulla At Nulla Justo, Eget</h5>
+        <p>$400</p>
+        <a href="#" class="button small expanded hollow">Buy</a>
+      </div>
+    </div>
+
+    <hr>
+    <div class="row">
+      <div class="medium-4 columns">
+        <h4>Top Products</h4>
+        <div class="media-object">
+          <div class="media-object-section">
+            <img class="thumbnail" src="http://placehold.it/100x100">
+          </div>
+          <div class="media-object-section">
+            <h5>Nunc Eu Ullamcorper Orci</h5>
+            <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+          </div>
+        </div>
+        <div class="media-object">
+          <div class="media-object-section">
+            <img class="thumbnail" src="http://placehold.it/100x100">
+          </div>
+          <div class="media-object-section">
+            <h5>Nunc Eu Ullamcorper Orci</h5>
+            <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+          </div>
+        </div>
+        <div class="media-object">
+          <div class="media-object-section">
+            <img class="thumbnail" src="http://placehold.it/100x100">
+          </div>
+          <div class="media-object-section">
+            <h5>Nunc Eu Ullamcorper Orci</h5>
+            <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+          </div>
+        </div>
+      </div>
+      <div class="medium-4 columns">
+        <h4>Top Products</h4>
+        <div class="media-object">
+          <div class="media-object-section">
+            <img class="thumbnail" src="http://placehold.it/100x100">
+          </div>
+          <div class="media-object-section">
+            <h5>Nunc Eu Ullamcorper Orci</h5>
+            <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+          </div>
+        </div>
+        <div class="media-object">
+          <div class="media-object-section">
+            <img class="thumbnail" src="http://placehold.it/100x100">
+          </div>
+          <div class="media-object-section">
+            <h5>Nunc Eu Ullamcorper Orci</h5>
+            <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+          </div>
+        </div>
+        <div class="media-object">
+          <div class="media-object-section">
+            <img class="thumbnail" src="http://placehold.it/100x100">
+          </div>
+          <div class="media-object-section">
+            <h5>Nunc Eu Ullamcorper Orci</h5>
+            <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+          </div>
+        </div>
+      </div>
+      <div class="medium-4 columns">
+        <h4>Top Products</h4>
+        <div class="media-object">
+          <div class="media-object-section">
+            <img class="thumbnail" src="http://placehold.it/100x100">
+          </div>
+          <div class="media-object-section">
+            <h5>Nunc Eu Ullamcorper Orci</h5>
+            <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+          </div>
+        </div>
+        <div class="media-object">
+          <div class="media-object-section">
+            <img class="thumbnail" src="http://placehold.it/100x100">
+          </div>
+          <div class="media-object-section">
+            <h5>Nunc Eu Ullamcorper Orci</h5>
+            <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+          </div>
+        </div>
+        <div class="media-object">
+          <div class="media-object-section">
+            <img class="thumbnail" src="http://placehold.it/100x100">
+          </div>
+          <div class="media-object-section">
+            <h5>Nunc Eu Ullamcorper Orci</h5>
+            <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="callout large secondary">
+      <div class="row">
+        <div class="large-4 columns">
+          <h5>Vivamus Hendrerit Arcu Sed Erat Molestie</h5>
+          <p>Curabitur vulputate, ligula lacinia scelerisque tempor, lacus lacus ornare ante, ac egestas est urna sit amet arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed molestie augue sit.</p>
+        </div>
+        <div class="large-3 large-offset-2 columns">
+          <ul class="menu vertical">
+            <li><a href="#">One</a></li>
+            <li><a href="#">Two</a></li>
+            <li><a href="#">Three</a></li>
+            <li><a href="#">Four</a></li>
+          </ul>
+        </div>
+        <div class="large-3 columns">
+          <ul class="menu vertical">
+            <li><a href="#">One</a></li>
+            <li><a href="#">Two</a></li>
+            <li><a href="#">Three</a></li>
+            <li><a href="#">Four</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
     <div id="app"></div>
     <!-- built files will be auto injected -->
   </body>
